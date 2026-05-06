3b47e92cdfe162f94e670fe68d22ebf48f88ed20
Lara Abreu <laraacfreitas@gmail.com>
2026-05-06 16:30:30 -0300

    Add files via upload

---
+++ Memory_Game/src/scripts/engine.js
@@ -0,0 +1,38 @@
+const emojis = ['🍎', '🍎', '🍌', '🍌', '🍇', '🍇', '🍉', '🍉', '🍓', '🍓', '🍒', '🍒', '🥝', '🥝', '🍍', '🍍'];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i=0; i < emojis.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'item';
+    box.innerHTML = shuffleEmojis[i]; 
+    document.querySelector('.game').appendChild(box); 
+    box.onclick = handleClick;
+}
+
+function handleClick() {
+    if (openCards.length < 2){
+        this.classList.add('boxOpen');
+        openCards.push(this);
+    } 
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }  
+}
+
+function checkMatch() {
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }
+    else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if (document.querySelectorAll("boxMatch").length === emojis.length) {
+        alert("Você ganhou :D")
+    }
+}
